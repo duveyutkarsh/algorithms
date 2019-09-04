e526d9722fcf08f147c9dfce858986ba386e8770
Utkarsh Duvey <duveyu@amazon.com>
2019-09-04 08:26:32 +0530

    Added a naive gcd function trying all divisors

--- gcd.cpp
+++ gcd.cpp
@@ -4,9 +4,9 @@ using namespace std;
 // O(log min(a , b))
 //recursive ----------------------------------------
 
-int gcd(int a, int b){
+int gcdr(int a, int b){
     if(b == 0)return a;
-    else return gcd(b , a%b);
+    else return gcdr(b , a%b);
 }
 
 // O(log min(a , b))
@@ -19,17 +19,32 @@ int gcd(int a, int b){
 // O(log min(a , b))
 // Iterative ---------------------------------------
 
-int gcd ( int a, int b ){
+int gcdi( int a, int b ){
     while (b){
-        a % = b ;
+        a %= b ;
         swap ( a, b ) ;
     }
     return a ;
 } 
 
+// O( sqrt(min(a,b)) )
+
+int gcdsq(int a ,int b){
+    if(a<b)swap(a , b);
+    if(b==0)return a;
+    //trying all divisor of b since gcd(a,b) will be bounded by divisors of b.
+    int ans = 1;
+    for(int div = 1 ; div*div <= b ; div++){
+        if(b%div != 0)continue;
+        if(a%div == 0)ans = max(ans , div);
+        if(a%(b/div) == 0)ans = max(ans , b/div);
+    }
+    return ans;
+}
+
 int main(int argc, char const *argv[])
 {
     ios::sync_with_stdio(0);cin.tie(0);
-    
+    cout << gcdsq(8 , 24);
     return 0;
 }
